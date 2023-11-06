4753e6c2c419bfda816bfc1edef1377d86ea2dd3
Shiva <shiva.khatri0001@gmail.com>
2023-11-06 12:20:18 -0500

    Some improvements

--- test.c
+++ test.c
@@ -10,13 +10,19 @@ int main(){
     char board[100]="  mHk tH l B He Flq p H  hByHlho H B  jr HFB ir j H FF ku gd  H pjB mH x  BF i H Bm oB HlHFBhoH BB ";
 
     char* player1 = board; // Initialize player 1 pointer
+    char*p = board;
+    int move = 2;
+    p = p + move;
+    
+    printf("This is the p %c\n", *p);
+
 
 
     printf("This is the address %p\n", player1);
     printf("This is the address %p\n", board);
 
     for(int i = 0; i < 100; i++){
-        printf("This is %d and character %c\n",i, *(player1+i));
+      //  printf("This is %d and character %c\n",i, *(player1+i));
     }
 
     return 0;
